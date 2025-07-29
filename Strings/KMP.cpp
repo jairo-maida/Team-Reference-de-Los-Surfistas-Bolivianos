7249ddd68143739406953a0eb31df81454a027ee
Winsor <winsoromar12@gmail.com>
2025-07-29 17:27:37 -0400

    fix: change int to ll in kmp

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for (int i = a; i < (b); ++i)
 typedef long long ll;
 
-vector<int> pi(const string& s) {
-    vector<int> p(s.size());
-    for (int i = 1; i < s.size(); i++) {
-        int g = p[i - 1];
+vector<ll> pi(const string& s) {
+    vector<ll> p(s.size());
+    for (ll i = 1; i < s.size(); i++) {
+        ll g = p[i - 1];
         while (g && s[i] != s[g])
             g = p[g - 1];
 
@@ -16,9 +15,9 @@ vector<int> pi(const string& s) {
     return p;
 }
 
-vector<int> match(const string& s, const string& pat) {
-    vector<int> p = pi(pat + '\0' + s), res;
-    for (int i = p.size() - s.size(); i < p.size(); i++) {
+vector<ll> match(const string& s, const string& pat) {
+    vector<ll> p = pi(pat + '\0' + s), res;
+    for (ll i = p.size() - s.size(); i < p.size(); i++) {
         if (p[i] == pat.size())
             res.push_back(i - 2 * pat.size());
     }
